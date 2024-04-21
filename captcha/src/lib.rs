5d374b2b3553394e170a57dc6be2607b069babed
axumrs <rolson867@gmail.com>
2024-04-21 14:59:16 +0800

    渐变

--- captcha/src/lib.rs
+++ captcha/src/lib.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod tests {
-    use image::RgbaImage;
+    use image::{imageops, Pixel, Rgba, RgbaImage};
 
     #[test]
     fn solid_color_bg() {
@@ -8,6 +8,20 @@ mod tests {
         let pixel = image::Rgba([255u8, 0u8, 0u8, 255u8]);
         // 创建图片缓冲区，并使用上面定义的像素点（颜色）作为背景填充整个图片
         let img = RgbaImage::from_fn(100, 100, |_, _| pixel);
+        // 保存
+        img.save("foo.png").unwrap();
+    }
+
+    #[test]
+    fn gradient_bg() {
+        let mut img = RgbaImage::new(100, 100);
+
+        // #22d3ee - cyan-400(tailwind css)
+        let start = Rgba::from_slice(&[0x22, 0xd3, 0xee, 0xff]);
+        // #164e63 - cyan-900(tailwind css)
+        let end = Rgba::from_slice(&[0x16, 0x43, 0x63, 0xff]);
+
+        imageops::vertical_gradient(&mut img, start, end);
         // 保存
         img.save("foo.png").unwrap();
     }
