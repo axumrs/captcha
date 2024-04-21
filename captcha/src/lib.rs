f343865f6cd0ae21a8247fca92b85dd488771f1b
axumrs <rolson867@gmail.com>
2024-04-21 14:49:34 +0800

    /图像处理及纯色背景图片

--- captcha/src/lib.rs
+++ captcha/src/lib.rs
@@ -1,14 +1,14 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use image::RgbaImage;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn solid_color_bg() {
+        // 像素（颜色）：红色，不透明
+        let pixel = image::Rgba([255u8, 0u8, 0u8, 255u8]);
+        // 创建图片缓冲区，并使用上面定义的像素点（颜色）作为背景填充整个图片
+        let img = RgbaImage::from_fn(100, 100, |_, _| pixel);
+        // 保存
+        img.save("foo.png").unwrap();
     }
 }
